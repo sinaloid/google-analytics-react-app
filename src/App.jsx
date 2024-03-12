bdb7da4dd818471c6a7f567c4b9e7f14a56a1497
Sinali Dionou <sinaloid@gmail.com>
2024-03-12 14:38:41 +0000

    react-ga install

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ReactGA from 'react-ga'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    ReactGA.initialize('G-LXVYZMKL8H')
+    ReactGA.pageview("/")
+  },[])
 
   return (
     <>
